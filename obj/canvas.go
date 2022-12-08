87bafb434fcb70b1c79e15dc42ec40d0456fe6c3
sam <sam-chen@eup.com.tw>
2022-12-08 13:50:59 +0800

    feat: update windows size

--- obj/canvas.go
+++ obj/canvas.go
@@ -7,7 +7,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 )
 
-const width, height float32 = 1920, 1080
+const width, height float32 = 1600, 1200
 
 type Canvases struct {
 	Instruction *canvas.Image
@@ -17,8 +17,8 @@ type Canvases struct {
 
 func (c *Canvases) Load(instructFile *material.InstructFile, pictureFile *material.PictureFile) {
 	c.Instruction = canvas.NewImageFromFile(instructFile.CurrentInstruction)
-	c.Instruction.SetMinSize(fyne.Size{Width: width * 0.8, Height: height * 0.8})
-	c.Instruction.FillMode = canvas.ImageFillStretch
+	c.Instruction.SetMinSize(fyne.Size{Width: width, Height: height})
+	c.Instruction.FillMode = canvas.ImageFillContain
 
 	c.Picture = canvas.NewImageFromFile(pictureFile.CurrentPicture)
 	c.Picture.SetMinSize(fyne.Size{Width: width * 0.7, Height: height * 0.7})
@@ -26,5 +26,5 @@ func (c *Canvases) Load(instructFile *material.InstructFile, pictureFile *materi
 
 	c.Mask = canvas.NewImageFromFile(pictureFile.Mask)
 	c.Mask.SetMinSize(fyne.Size{Width: width, Height: height})
-	c.Mask.FillMode = canvas.ImageFillStretch
+	c.Mask.FillMode = canvas.ImageFillContain
 }
